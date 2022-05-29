311f592e6e4957dbbcfbca8b965a5adc41f37c3b
Andrei Voicu <andrei.voicu110@gmail.com>
2022-05-29 22:22:35 +0300

    Update

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   withCredentials: true,
-  baseURL: "https://quicktalk-web-app.herokuapp.com/api",
+  baseURL: "https://quicktalk.andreiv03.vercel.app/api",
   headers: {
     "Content-Type": "application/json"
   }
--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://quicktalk-web-app.herokuapp.com/");
+const socket = io("https://quicktalk.andreiv03.vercel.app/");
 export default socket;
