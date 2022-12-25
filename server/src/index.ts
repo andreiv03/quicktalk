09de9effb7e1c66c8ce727859c1e94703c2f8a59
Andrei Voicu <andrei.voicu110@gmail.com>
2022-12-25 12:04:40 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,21 +17,15 @@ app.use(express.json());
 app.use(cors(options));
 app.use(cookieParser());
 app.use(routes);
-
-switch (process.env.NODE_ENV) {
-  case "production":
-    app.use(express.static(path.join(path.resolve(), "/client/build")));
-    app.get("*", (req, res) => res.sendFile(path.join(path.resolve(), "client", "build", "index.html")));
-    break;
-
-  default: app.get("*", (req, res) => res.send("Server is running!"));
-}
+app.get("*", (req, res) => res.send("Server is running!"));
 
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, { cors: options });
 
-httpServer.listen(PORT, () => {
-  socket(io);
-  connectToMongoDB();
-  console.log(`Server is running on port ${PORT}`);
-}).on("error", () => process.exit(1));
+httpServer
+  .listen(PORT, () => {
+    socket(io);
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+  })
+  .on("error", () => process.exit(1));
