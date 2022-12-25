0f27eeda0f1b49e340e79580394d29203c8084d7
Andrei Voicu <andrei.voicu110@gmail.com>
2022-12-25 13:09:41 +0200

    Update index.ts

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 if (process.env.NODE_ENV !== "production") dotenv.config();
 
 const PORT = process.env.PORT || "5000";
-const CLIENT_URL = process.env.CLIENT_URL || "https://quicktalk-client.vercel.app";
+const CLIENT_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://quicktalk-client.vercel.app";
 const MONGODB_URI = process.env.MONGODB_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
 
