e2b84596dbee0d5ee850341457b9a43f67961329
Andrei Voicu <andrei.voicu110@gmail.com>
2022-12-25 11:47:26 +0200

    New

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 
 export default axios.create({
   withCredentials: true,
-  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5000/api" : "https://quicktalk-andreiv03.herokuapp.com/api",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5000/api"
+      : "https://quicktalk-andreiv03.herokuapp.com/api",
   headers: {
     "Content-Type": "application/json"
   }
--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,4 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://quicktalk-andreiv03.herokuapp.com");
+const socket = io(
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://quicktalk-andreiv03.herokuapp.com"
+);
 export default socket;
