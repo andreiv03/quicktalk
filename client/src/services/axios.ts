7115e9710052618b2f2b6685d7e177e3822867fd
Andrei Voicu <andrei.voicu110@gmail.com>
2022-12-25 12:02:54 +0200

    Fix

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+console.log(process.env);
+
 export default axios.create({
   withCredentials: true,
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:5000/api"
-      : `${process.env.SERVER_URL!}/api`,
+      : `${process.env.REACT_APP_SERVER_URL!}/api`,
   headers: {
     "Content-Type": "application/json"
   }
--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  process.env.NODE_ENV === "development" ? "http://localhost:5000" : process.env.SERVER_URL!
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : process.env.REACT_APP_SERVER_URL!
 );
 export default socket;
